b862c615fed970311669f28bfe3402181925dc19
Dikran H <dhachikian@expert-bg.org>
2020-10-13 10:10:46 +0300

    Python L01

---
+++ ex03-strings.py
@@ -0,0 +1,31 @@
+# %%
+
+s1 = 'Lorem ipsum dolor sit amet'
+s1
+# %%
+s2 = "Lorem ipsum dolor sit amet"
+s2
+# %%
+s3 = '''Ut enim ad minim veniam,
+quis nostrud exercitation ullamco 
+laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in 
+reprehenderit'''
+s3
+# %%
+print(sorted.__doc__)
+# %%
+x = 12
+
+print(f'Стойността на x = {x}')
+# %%
+s1
+# %%
+s1[2]
+# %%
+s1[0:5]
+# %%
+s1[::2]
+# %%
+s1[::-1]
+# %%
